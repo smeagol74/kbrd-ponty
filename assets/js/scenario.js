d3c3336df56a88ffb64d79ba0b5378b644b61dc6
Konstantin Borisov <smeagol74@gmail.com>
2015-09-25 12:27:00 +0600

    tiling sprites

--- assets/js/scenario.js
+++ assets/js/scenario.js
@@ -16,27 +16,15 @@ var scenario = {
 
 (function(){
 
-    function addNumber(_layout, _image) {
-        var result = app.add.sprite(_layout.x + _layout.w / 2, _layout.y + _layout.h / 2, _image);
-        result.anchor.setTo(0.5, 0.5);
-        return result;
-    }
-
     scenario.init = function(){
-        counter.left.a.y = counter.left.a.y - layout.counter.left.a.h * 10;
-        counter.left.b.y = counter.left.b.y - layout.counter.left.b.h * 10;
-        counter.left.c.y = counter.left.c.y - layout.counter.left.c.h * 10;
-        counter.right.a.y = counter.right.a.y - layout.counter.right.a.h * 10;
-        counter.right.b.y = counter.right.b.y - layout.counter.right.b.h * 10;
-        counter.right.c.y = counter.right.c.y - layout.counter.right.c.h * 10;
     };
 
     scenario.stages = [
         {
             play: function(){
                 console.log('Шаг 1');
-                var tween = app.add.tween(counter.left.c);
-                tween.to({y: counter.left.c.y + layout.counter.left.c.h * 3}, 2500, Phaser.Easing.Elastic.Out, true);
+                var tween = app.add.tween(counter.left.c.tilePosition);
+                tween.to({y: counter.left.c.tilePosition.y + layout.counter.left.c.h * 3}, 2500, Phaser.Easing.Elastic.Out, true);
                 snd.money.play();
             },
             update: function(){
@@ -46,8 +34,8 @@ var scenario = {
         {
             play: function(){
                 console.log('Шаг 1');
-                var tween = app.add.tween(counter.left.c);
-                tween.to({y: counter.left.c.y + layout.counter.left.c.h}, 2500, Phaser.Easing.Elastic.Out, true);
+                var tween = app.add.tween(counter.left.c.tilePosition);
+                tween.to({y: counter.left.c.tilePosition.y + layout.counter.left.c.h}, 2500, Phaser.Easing.Elastic.Out, true);
                 snd.money.play();
             },
             update: function(){
@@ -57,8 +45,8 @@ var scenario = {
         {
             play: function(){
                 console.log('Шаг 1');
-                var tween = app.add.tween(counter.left.c);
-                tween.to({y: counter.left.c.y + layout.counter.left.c.h}, 1000, Phaser.Easing.Elastic.Out, true);
+                var tween = app.add.tween(counter.left.c.tilePosition);
+                tween.to({y: counter.left.c.tilePosition.y + layout.counter.left.c.h}, 1000, Phaser.Easing.Elastic.Out, true);
                 snd.money.play();
             },
             update: function(){
@@ -68,8 +56,8 @@ var scenario = {
         {
             play: function(){
                 console.log('Шаг 1');
-                var tween = app.add.tween(counter.left.b);
-                tween.to({y: counter.left.b.y + layout.counter.left.b.h}, 5000, Phaser.Easing.Elastic.Out, true);
+                var tween = app.add.tween(counter.left.b.tilePosition);
+                tween.to({y: counter.left.b.tilePosition.y + layout.counter.left.b.h}, 5000, Phaser.Easing.Elastic.Out, true);
                 snd.money.play();
             },
             update: function(){
